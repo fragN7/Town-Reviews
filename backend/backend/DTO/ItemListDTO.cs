ec2f2532f82fc073f9b12743727b5c0a7663c3a9
Alen <132235388+fragN7@users.noreply.github.com>
2023-07-05 22:39:18 +0300

    Added image and image frame

--- backend/backend/DTO/ItemListDTO.cs
+++ backend/backend/DTO/ItemListDTO.cs
@@ -4,7 +4,7 @@ public class ItemListDTO
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public float Rating { get; set; }
+    public float? Rating { get; set; } = new float();
     public string Description { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
     public ICollection<AddReviewDTO> Reviews { get; set; } = null!;
